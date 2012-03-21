476681bd237111fff1883935e22014957e360a47
Dexter Tan <dexter.tan.2009@smu.edu.sg>
2012-03-21 13:30:08 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friday::Application.config.secret_token = '0f07dbc959b3a03d42c10e43dd1e99233ee08ba5d37877f67c483a362e70160249fb975a792ce368f23121ee0acd8354ed69aafcc73d2e35f4b75156c63b81a5'
