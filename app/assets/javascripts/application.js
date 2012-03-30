8ea0b22b716f87fb30732c415b94bbef7d3a13b4
unknown <Dexter@Dexter-i5.(none)>
2012-03-30 22:18:55 +0800

    minor tweaks to application layout to add links to footer items and added facebook Javascript SDK and a facebook login button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,20 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// Facebook Authentication Javascript SDK
+window.fbAsyncInit = function() {
+  FB.init({
+	appId      : '199437970167670',
+	status     : true, 
+	cookie     : true,
+	xfbml      : true,
+	oauth      : true,
+  });
+};
+(function(d){
+   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   d.getElementsByTagName('head')[0].appendChild(js);
+ }(document));
