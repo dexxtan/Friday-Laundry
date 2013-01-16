51e830cd65100cb43e8444ce5cdb58754ff7908f
Dexter Tan <mr.aves@gmail.com>
2013-01-16 18:32:58 +0800

    updated facebook code to newer version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,17 +16,51 @@
 
 // Facebook Authentication Javascript SDK
 window.fbAsyncInit = function() {
-  FB.init({
-	appId      : '199437970167670',
-	status     : true, 
-	cookie     : true,
-	xfbml      : true,
-	oauth      : true,
-  });
+    FB.init({
+            appId      : '199437970167670',
+            status     : true, 
+            cookie     : true,
+            xfbml      : true,
+            oauth      : true,
+            channelUrl : "//friday-dev.herokuapp.com/channel.html"
+    });
+  
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+          // connected
+        } else if (response.status === 'not_authorized') {
+          // not_authorized
+          login();
+        } else {
+          // not_logged_in
+          login();
+        }
+    });
 };
+
+// Load the SDK Asynchronously
 (function(d){
-   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
    js = d.createElement('script'); js.id = id; js.async = true;
    js.src = "//connect.facebook.net/en_US/all.js";
-   d.getElementsByTagName('head')[0].appendChild(js);
+   ref.parentNode.insertBefore(js, ref);
  }(document));
+ 
+function login() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            // connected
+            testAPI();
+        } else {
+            // cancelled
+        }
+    });
+}
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+    });
+}
