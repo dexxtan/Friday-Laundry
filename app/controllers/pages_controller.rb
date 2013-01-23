cba1a8ea2dda59d1b2f2046c8ef39a070fb005a7
Dexter Tan <mr.aves@gmail.com>
2013-01-23 18:41:41 +0800

    created a filter to check for user logins, added skip filters for pages that do not need login to access and set session variables after users are created to 'login' a user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_filter :require_login, :only => [:home, :signup]
   def home
   end
 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,11 @@
 class UserController < ApplicationController
+  skip_before_filter :require_login, :only => [:create, :fbcreate]
   def create
     @user = User.new
     @user.email = params[:email]
     @user.save
+    
+    session[:current_user_id] = @user.id
     
     redirect_to "/signup"
   end
@@ -11,6 +14,8 @@ class UserController < ApplicationController
     @user = User.new
     @user.name = params[:name]
     @user.save
+    
+    session[:current_user_id] = @user.id
     
     render :json => @user
   end
